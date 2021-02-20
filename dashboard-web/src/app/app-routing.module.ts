68a7b52bd6bc55d4fcacf767ff47fbd01d90f6d0
Sayantan Ghosh <suv@ip-7-0-76-65.eu-west-2.compute.internal>
2021-02-20 13:25:55 +0000

    fresh

--- dashboard-web/src/app/app-routing.module.ts
+++ dashboard-web/src/app/app-routing.module.ts
@@ -1,19 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { PanelsComponent } from './panels/panels.component';
-import { HomeComponent } from './home/home.component';
-import { ExecutorComponent } from './executor/executor.component';
-import { ContactComponent } from "./contact/contact.component";
-import { SupportComponent } from "./support/support.component";
 
-const routes: Routes = [
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: 'home', component: HomeComponent },
-  { path: 'panels', component: PanelsComponent },
-  { path: 'executor', component: ExecutorComponent },
-  { path: 'contact', component: ContactComponent},
-  { path: 'support', component: SupportComponent}
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
