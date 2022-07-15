0932d49ba0a067d4faa866bdbd27eb4090ac67d9
Your Name <you@example.com>
2022-07-15 18:20:09 +0000

    it works

--- dashboard-web/src/app/home/home.component.ts
+++ dashboard-web/src/app/home/home.component.ts
@@ -2,7 +2,7 @@ import {AfterViewInit, Component, OnInit} from '@angular/core';
 
 
 @Component({
-  selector: 'app-home',
+  //selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
